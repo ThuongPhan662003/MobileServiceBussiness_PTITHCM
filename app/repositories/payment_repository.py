790e08fad3b723d0f969bea1e03541bcfacd0e17
ThuongPhan662003 <thuong5so55555@gmail.com>
2025-04-25 01:29:09 +0700

    add qr code in admin payment history and fixx pament repo

--- app/repositories/payment_repository.py
+++ app/repositories/payment_repository.py
@@ -197,3 +197,25 @@ class PaymentRepository:
         except Exception as e:
             logger.error(f"Lỗi khi tìm kiếm payment: {e}")
             return []
+
+    @staticmethod
+    def create_full_payment_transaction(plan_code: str, subscriber_id: int):
+        try:
+            result = db_instance.execute(
+                "CALL CreateFullPaymentTransaction(%s, %s)",
+                (plan_code, subscriber_id),
+                fetchone=True,
+                commit=True,
+            )
+            if result.get("success"):
+                return {
+                    "success": True,
+                    "message": result.get("message"),
+                    "subscription_id": result.get("subscription_id"),
+                    "payment_id": result.get("payment_id"),
+                }
+            else:
+                return {"success": False, "message": result.get("message")}
+        except Exception as e:
+            print(f"[Repository] Lỗi khi tạo giao dịch thanh toán đầy đủ: {e}")
+            return {"success": False, "message": str(e)}
