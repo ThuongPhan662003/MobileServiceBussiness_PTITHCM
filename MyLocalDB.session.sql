84dc14231d9e313a980dda9c52343fe933625080
ThuongPhan662003 <thuong5so55555@gmail.com>
2025-06-08 22:21:00 +0700

    fix infor staff

--- MyLocalDB.session.sql
+++ MyLocalDB.session.sql
@@ -1,1 +1,25 @@
-CALL GetStaffById(7);
+-- Quản lý
+INSERT INTO `permissiondetail` (`role_group_id`, `account_id`) VALUES
+(1, 10),
+(1, 11);
+
+-- Nhân viên
+INSERT INTO `permissiondetail` (`role_group_id`, `account_id`) VALUES
+(2, 12),
+(2, 13),
+(2, 14),
+(2, 15),
+(2, 16),
+(2, 17),
+(2, 18),
+(2, 19);
+
+-- Thuê bao
+INSERT INTO `permissiondetail` (`role_group_id`, `account_id`) VALUES
+(3, 20),
+(3, 21),
+(3, 22),
+(3, 23),
+(3, 24),
+(3, 25),
+(3, 26);
